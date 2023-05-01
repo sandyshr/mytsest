aa108e40e2716f8a0ceba87f13f4b24252d323f5
sandysh <sandyshr@gmail.com>
2023-05-01 02:31:41 -0400

    fixed threding

--- example.c
+++ example.c
@@ -11,43 +11,51 @@
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+
+TargetList *my_target_list;
+
 void ping_check(void *arg) {
-    pthread_mutex_lock(&mutex);
-    TargetList *target_list = (TargetList *) arg;
-    for (int i = 0; i < target_list->target_count; i++) {
-        Target *target = &target_list->targets[i];
+    //TargetList *target_list = (TargetList *) arg;
+    // for (int i = 0; i < target_list->target_count; i++) {
+        Target *target = (Target *)arg;
         if (target->status == UNKNOWN) {
             char cmd[256];
-            sprintf(cmd, "ping -c 1 -W 1 %s > /dev/null", inet_ntoa(target->ip_addr));
+            sprintf(cmd, "ping -c 1 -w 1 %s > /dev/null", inet_ntoa(target->ip_addr));
             FILE *fp = popen(cmd, "r");
             if (fp == NULL) {
                 printf("Failed to execute command for %s\n", inet_ntoa(target->ip_addr));
-                continue;
+                //continue;
             }
             int ret = pclose(fp);
             if (ret == 0) {
+				pthread_mutex_lock(&mutex);
                 target->status = UP;
                 printf("%s is UP\n", inet_ntoa(target->ip_addr));
+				    pthread_mutex_unlock(&mutex);
+
             } else {
+				    pthread_mutex_lock(&mutex);
+
                 target->status = DOWN;
                 printf("%s is DOWN\n", inet_ntoa(target->ip_addr));
+				    pthread_mutex_unlock(&mutex);
+
             }
         }
-    }
-    pthread_mutex_unlock(&mutex);
+    //}
 }
 
 
 int main(int argc, char* argv[]){
-    char ip_input[BUF_SIZE] = "192.168.127.128/30";
-    TargetList *my_target_list = get_target_list(ip_input);
+    char ip_input[BUF_SIZE] = "192.168.127.128/26";
+    my_target_list = get_target_list(ip_input);
 
     puts("Making threadpool with 4 threads");
-    threadpool thpool = thpool_init(4);
+    threadpool thpool = thpool_init(8);
 
     puts("Adding tasks to threadpool");
     for (int i = 0; i < my_target_list->target_count; i++){
-        thpool_add_work(thpool, ping_check, my_target_list);
+        thpool_add_work(thpool, ping_check, &(my_target_list->targets[i]));
     }
 
     thpool_wait(thpool);
